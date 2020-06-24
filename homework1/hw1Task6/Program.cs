e067d5936174fd3682299c4a9fe0464b91729838
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-06-24 16:16:55 +0300

    homework1 is ready

---
+++ homework1/hw1Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//6.	* Создать класс с методами, которые могут пригодиться в вашей учебе(Print, Pause).
+//Рощупкин
+namespace hw1Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 10, c = 10;
+            double x1, x2;
+            UsefulMethods.SquareEqualSol(a, b, c, out x1, out x2);
+            Console.WriteLine($"x1 = {x1:N}, x2 = {x2:N}");
+            Console.ReadLine();
+
+            Console.Clear();
+            int length = 10;
+            string password = UsefulMethods.GeneratePassword(length);
+            Console.WriteLine("Сгенерированный пароль: " + password);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework1/hw1Task6/UsefulMethods.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace hw1Task6
+{
+    static class UsefulMethods
+    {
+        public static void SquareEqualSol(double a, double b, double c, out double x1, out double x2)
+        {
+            if (a == 0)
+            {
+                x1 = -c / b;
+                x2 = Double.NaN;
+            }
+            else
+            {
+                double D = b * b - 4 * a * c;
+                if (D == 0)
+                {
+                    x1 = -b / (2 * a);
+                    x2 = Double.NaN;
+                }
+                else if (D > 0)
+                {
+                    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                }
+                else
+                {
+                    x1 = x2 = Double.NaN;
+                }
+            }
+        }
+
+        internal static string GeneratePassword(int length)
+        {
+            Random rnd = new Random();
+            string password = "";
+            for (int i = 0; i < length; i++)
+            {
+                int n = rnd.Next(0, 4);
+                char[] spec_chars = { '!', '@', '?', '#', '$', '%', '&' };
+
+                switch (n)
+                {
+                    case 0:
+                        {
+                            password += rnd.Next(10);
+                            break;
+                        }
+                    case 1:
+                        {
+                            password += Convert.ToChar(rnd.Next(65, 88));
+                            break;
+                        }
+                    case 2:
+                        {
+                            password += Convert.ToChar(rnd.Next(97, 122));
+                            break;
+                        }
+                    default:
+                        {
+                            password += spec_chars[rnd.Next(spec_chars.Length)];
+                            break;
+                        }
+                }
+            }
+            return password;
+        }
+    }
+}
