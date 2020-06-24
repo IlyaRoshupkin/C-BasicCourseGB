7cf5248ebfdb3c3c5a353e89b395b419c9e07464
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-06-24 16:22:31 +0300

    new version

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+/*1.	Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+а) используя склеивание;
+б) используя форматированный вывод;
+	в) используя вывод со знаком $.*/
+//Рощупкин
+
+namespace hw1Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> data = Ask();
+            GetInformation1Var(data);
+            GetInformation2Var(data);
+            GetInformation3Var(data);
+            Console.ReadLine();
+        }
+
+        private static void GetInformation3Var(Dictionary<string,string> data)
+        {
+            Console.WriteLine($"в)\nИмя: {data["firstName"]} Фамилия: {data["lastName"]}" +
+                $" Возраст: {data["age"]} Рост: {data["height"]} см Вес: {data["weight"]} кг");
+        }
+
+        private static void GetInformation2Var(Dictionary<string, string> data)
+        {
+            Console.WriteLine("б)\nИмя: {0} Фамилия: {1} Возраст: {2} Рост: {3} см Вес: {4} кг",
+                data["firstName"], data["lastName"], data["age"], data["height"], data["weight"]);
+        }
+
+        private static void GetInformation1Var(Dictionary<string, string> data)
+        {
+            Console.WriteLine("а)\nИмя: " + data["firstName"] + " Фамилия: " + data["lastName"] +
+                " Возраст: " + data["age"] +
+                " Рост: " + data["height"] + " см Вес: " + data["weight"] + " кг");
+        }
+
+        private static Dictionary<string,string> Ask()
+        {
+            var data = new Dictionary<string, string>();
+            Console.Write("Введите Ваше имя: ");
+            data.Add("firstName", Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write($"{data["firstName"]}, введите Вашу фамилию: ");
+            data.Add("lastName", Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write($"{data["firstName"]} {data["lastName"]}, введите Ваш возраст: ");
+            data.Add("age", Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write($"{data["firstName"]} {data["lastName"]}, введите Ваш рост (см): ");
+            data.Add("height", Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write($"{data["firstName"]} {data["lastName"]}, введите Ваш вес (кг): ");
+            data.Add("weight", Console.ReadLine());
+            Console.WriteLine();
+
+            return data;
+        }
+    }
+}
---
+++ homework1/hw1Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//3.	а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2).Вывести результат, используя спецификатор формата .2f(с двумя знаками после запятой);
+//б) * Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+//Рощупкин
+
+namespace hw1Task3
+{
+    class Program
+    {
+        private static double x1, y1, x2, y2;
+
+        static void Main(string[] args)
+        {
+            GetPoints();
+            // a) double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Расстояние между точками: {0:F2}",/* a) dist*/ CalculateDistance(x1, y1, x2, y2));
+            Console.ReadLine();
+        }
+
+        private static double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        private static void GetPoints()
+        {
+            bool correctX1, correctY1, correctX2, correctY2;
+            do
+            {
+                Console.Write("Введите координаты точки 1:\n X = ");
+                correctX1 = double.TryParse(Console.ReadLine(), out x1);
+
+                Console.Write(" Y = ");
+                correctY1 = double.TryParse(Console.ReadLine(), out y1);
+
+                Console.Write("\nВведите координаты точки 2:\n X = ");
+                correctX2 = double.TryParse(Console.ReadLine(), out x2);
+
+                Console.Write(" Y = ");
+                correctY2 = double.TryParse(Console.ReadLine(), out y2);
+            }
+            while (!correctX1 || !correctY1 || !correctY1 || !correctY2);
+        }
+    }
+}
