de75957c5948be167e001af7e3ffe9902423c29d
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-07-10 12:33:42 +0300

    it`s ready

---
+++ homework5/hw5task2/Message.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw5task2
+{
+    static class Message
+    {
+        static StringBuilder stringBuilder;
+
+        public static List<string> GetWordsList(string message)
+        {
+            List<string> listWords = new List<string>();
+            if (message != "")
+            {
+                try
+                {
+                    string[] words = message.Split(' ');
+
+                    foreach (var word in words)
+                    {
+                        string newWord = "";
+                        foreach (var letter in word)
+                            if (char.IsLetterOrDigit(letter))
+                                newWord += letter;
+                        listWords.Add(newWord.ToLower());
+                    }
+                }
+                catch(Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+            else
+                MessageBox.Show("Сообщение не может быть пустым!");
+            return listWords;
+        }
+
+        public static List<string> GetWordsList(string message, int length)
+        {
+            List<string> wordsList = GetWordsList(message);
+            List<string> newWordsList = new List<string>();
+            try
+            {
+                foreach (var word in wordsList)
+                    if (word.Length <= length)
+                        newWordsList.Add(word);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            return newWordsList;
+        }
+
+        public static void PrintWords(List<string> listWords)
+        {
+            foreach (var word in listWords)
+                Console.Write(word + " ");
+        }
+
+        public static List<string> GetWordsList(string message, char symbol)
+        {
+            List<string> wordsList = GetWordsList(message);
+            List<string> newWordsList = new List<string>();
+            try
+            {
+                foreach (var word in wordsList)
+                    if (word[word.Length - 1] != symbol)
+                        newWordsList.Add(word);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            return newWordsList;
+        }
+
+        public static List<string> GetTheLongestWords(string message)
+        {
+            List<string> theLongestWords = new List<string>();
+            try
+            {
+                List<string> wordsList = GetWordsList(message);
+                int max = 0;
+                foreach (var word in wordsList)
+                {
+                    if (word.Length > max)
+                    {
+                        max = word.Length;
+                        theLongestWords.Add(word.ToLower());
+                    }
+                    else if (word.Length == max)
+                        if (!theLongestWords.Contains(word.ToLower()))
+                            theLongestWords.Add(word);
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            return theLongestWords;
+        }
+
+        public static string GetTheLongestWordsLine(string message)
+        {
+            stringBuilder = new StringBuilder();
+            try
+            {
+                List<string> wordsList = GetWordsList(message);
+                List<string> theLongestWords = GetTheLongestWords(message);
+                foreach (var word in theLongestWords)
+                    stringBuilder.Append(word + " ");
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            return stringBuilder.ToString();
+        }
+
+        public static Dictionary<string,int> MakeFrequencyDict(string[] words,string text)
+        {
+            Dictionary<string, int> freqDict = GetKeys(words);
+            try
+            {
+                List<string> wordsList = GetWordsList(text);
+                foreach (string word in words)
+                {
+                    for (int i = 0; i < wordsList.Count; i++)
+                        if (word.ToLower() == wordsList[i])
+                            freqDict[word.ToLower()]++;
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            return freqDict;
+        }
+
+        private static Dictionary<string,int> GetKeys(string[] words)
+        {
+            Dictionary<string, int> freqDict = new Dictionary<string, int>();
+            try
+            {
+                foreach (var word in words)
+                    if (!freqDict.ContainsKey(word.ToLower()))
+                        freqDict.Add(word.ToLower(), 0);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            return freqDict;
+        }
+
+        public static void PrintWords(Dictionary<string,int> freqDict)
+        {
+            foreach (var word in freqDict.Keys)
+                Console.WriteLine(word + " : " + freqDict[word]);
+        }
+    }
+}
---
+++ homework5/hw5task4/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//4. * Задача ЕГЭ.
+// На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов некоторой средней
+//школы.В первой строке сообщается количество учеников N, которое не меньше 10, но не
+//превосходит 100, каждая из следующих N строк имеет следующий формат:
+//<Фамилия> <Имя> <оценки>,
+//где<Фамилия> — строка, состоящая не более чем из 20 символов, <Имя> — строка, состоящая не
+//более чем из 15 символов, <оценки> — через пробел три целых числа, соответствующие оценкам по
+//пятибалльной системе. <Фамилия> и<Имя>, а также<Имя> и<оценки> разделены одним пробелом.
+//Пример входной строки:
+//Иванов Петр 4 5 3
+//Требуется написать как можно более эффективную программу, которая будет выводить на экран
+//фамилии и имена трёх худших по среднему баллу учеников. Если среди остальных есть ученики,
+//набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
+//    Рощупкин
+namespace hw5task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> averagePoints = CreateAvrPointsDict("ExamsResult.txt");
+            
+
+            string[] theWorst = GetThreeTheWorst(averagePoints);
+            
+            foreach(var people in theWorst)
+            {
+                Console.WriteLine(people);
+            }
+            Console.ReadLine();
+        }
+
+        private static string[] GetThreeTheWorst(Dictionary<string, double> averagePoints)
+        {
+            string[] theWorst = new string[3];
+            double[] minPoints = new double[3];
+            for (int i = 0; i < minPoints.Length; i++)
+                minPoints[i] = double.MaxValue;
+            foreach (string people in averagePoints.Keys)
+            {
+                if (averagePoints[people] < minPoints[0])
+                    ArraysMovingOnThree(theWorst, minPoints,averagePoints,people);                  
+                else if (averagePoints[people] == minPoints[0])
+                    theWorst[0] = theWorst[0].Substring(0,theWorst[0].IndexOf(' ')) 
+                        + " " + people.Split(' ')[0] + " " + averagePoints[people];
+                else if (averagePoints[people] < minPoints[1])
+                    ArraysMovingOnTwo(theWorst, minPoints, averagePoints, people);            
+                else if (averagePoints[people] == minPoints[1])
+                    theWorst[1] = theWorst[1].Substring(0, theWorst[1].IndexOf(' '))
+                        + " " + people.Split(' ')[0] + " " + averagePoints[people];
+                else if (averagePoints[people] < minPoints[2])
+                { 
+                    minPoints[2] = averagePoints[people];
+                    theWorst[2] = people.Split(' ')[0] + " " + averagePoints[people];
+                }
+                else if (averagePoints[people] == minPoints[2])
+                    theWorst[2] = theWorst[2].Substring(0, theWorst[2].IndexOf(' '))
+                       + " " + people.Split(' ')[0] + " " + averagePoints[people];
+            }
+            return theWorst;
+        }
+
+        private static void ArraysMovingOnTwo(string[] theWorst, double[] minPoints, Dictionary<string, double> averagePoints, string people)
+        {
+            minPoints[2] = minPoints[1];
+            minPoints[1] = averagePoints[people];
+            theWorst[2] = theWorst[1];
+            theWorst[1] = people.Split(' ')[0] + " " + averagePoints[people];
+        }
+
+        private static void ArraysMovingOnThree(string[] theWorst, double[] minPoints, Dictionary<string, double> averagePoints, string people)
+        {
+            minPoints[2] = minPoints[1];
+            minPoints[1] = minPoints[0];
+            minPoints[0] = averagePoints[people];
+            theWorst[2] = theWorst[1];
+            theWorst[1] = theWorst[0];
+            theWorst[0] = people.Split(' ')[0] + " " + averagePoints[people];
+        }
+
+        private static Dictionary<string, double> CreateAvrPointsDict(string path)
+        {
+            Dictionary<string, double> averagePoints = new Dictionary<string, double>();
+            if (!File.Exists(path))
+                MessageBox.Show("Файл " + path + "не найден.");
+            else
+            {
+                try
+                {
+                    StreamReader str = new StreamReader(path);
+                    Regex correctLine = new Regex(@"[А-ЯЁ][а-яё]{2,19}\s[А-ЯЁ][а-яё]{2,14}\s[2-5]\s[2-5]\s[2-5]");
+                    int numberOfPeoples = int.Parse(str.ReadLine());
+                    if (numberOfPeoples < 10 || numberOfPeoples > 100)
+                        MessageBox.Show("Некорректное количество учеников.");
+                    else
+                    {
+                        for (int i = 0; i < numberOfPeoples; i++)
+                        {
+                            string line = str.ReadLine();
+                            if (correctLine.IsMatch(line))
+                            {
+                                string[] lines = line.Split(' ');
+                                double avrPoint = Math.Round(
+                                    (double.Parse(lines[2]) + double.Parse(lines[3]) + double.Parse(lines[4])) / 3, 2);
+                                averagePoints.Add(lines[0] + " " + lines[1], avrPoint);
+                            }
+                            else
+                                MessageBox.Show("Ошибка при считывании файла.\nСтрока № " + i +
+                                    ": " + line);
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message );
+                }
+            }
+            return averagePoints;
+        }
+    }
+}
